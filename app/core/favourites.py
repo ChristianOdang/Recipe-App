9d62a28cc425c5806e71c53f3811457e6c3a192b
JohnsonOdang <johnnyodanny@gmail.com>
2024-01-29 04:27:24 -0800

    module for fav CRUD

---
+++ app/core/favourites.py
@@ -0,0 +1,52 @@
+''' 
+Module for favourite CRUD
+'''
+
+from core.db import session
+from core.model import Favourite
+
+
+def getfirst_item():
+    # call the session with model as param
+    results = session.query(Favourite).first()
+
+    # close session
+    session.close()
+
+    # return object
+    return results
+
+
+def get_all():
+    # call the session with model as param
+    results = session.query(Favourite).all()
+
+    # close db connection
+    session.close()
+
+    # return DB object
+    return results
+
+
+def add_favourite(favourite_uri):
+    # use mode to get table details
+    fav = Favourite(favourites=favourite_uri)
+
+    # use the add method to add param
+    session.add(fav)
+
+    # commit changes
+    session.commit()
+
+
+def remove_uri(uri_id):
+    # call get element and delete
+    session.query(Favourite).filter(Favourite.favourites == uri_id).delete()
+    session.commit()
+
+
+def get_item(id):
+    results = session.query(Favourite).filter(
+        Favourite.favourites == id).first()
+    session.close()
+    return results
