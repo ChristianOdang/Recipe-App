8e154656b47952d177ba29dc156ad6a411276505
JohnsonOdang <johnnyodanny@gmail.com>
2024-02-04 08:01:43 -0800

    route for page details

--- app/core/routes.py
+++ app/core/routes.py
@@ -15,3 +15,56 @@ def index():
     return 'This is bp chekcks'
 
 # routes for index page
+@bp.route('/')
+def hello():
+    
+    try:
+        # call the query api method
+        data = querry_api()
+        
+        # pass data to view
+        return render_template('recipe/index.html',results = data)
+    except:
+        return render_template('error/404.html')
+    
+# route for homepage (index)
+@bp.route('/home')
+def home():
+    try:
+        data = querry_api()
+        
+        return render_template('recipe/index.html', results = data)
+    except:
+        return render_template('error/404.html')
+    
+# for test during dev remove before deployment
+@bp.route('/test')
+def test():
+    
+    data = querry_api()
+    return render_template('recipe/test.html', results = data)
+
+# route for page details
+@bp.route('/details', methods=['POST'])
+def detail():
+    
+    # get form data from form
+    uri = request.form['item-details']
+    
+    try:
+        # pass the param to the api
+        data = querry_uri(uri)
+        
+        # return view and data
+        return render_template('recipe/detail.html', results = data)
+    except:
+        return render_template('error/404.html')
+
+
+# cart object for post
+@bp.route('/cart', methods=['POST'])
+def cart():
+    data = request.get_json()
+    
+    return redirect(url_for('core.cart_display', data = data))
+
